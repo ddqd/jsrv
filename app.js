19b15ba4e8844fd7b694a6fbf5bf53a8f28285cf
Dmitry Gureev <dmi.gureev@gmail.com>
2013-11-07 13:21:25 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , path = require('path')
+  , server = http.createServer(app);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+// app.use(express.favicon(__dirname + "/app/public/images/favicon.ico"));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+app.use(express.static(__dirname + "/app/public"));
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var config = require('./config.json');
+
+require(__dirname+'/app/router')(app, config);
+
+server.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ app/router.js
@@ -0,0 +1,16 @@
+module.exports = function(app, config) {
+
+	function getState() {
+		if (this.num == undefined) {
+			this.num = (this.num || 0) + 0;
+		} 
+		var ind = this.num;
+		(this.num >= config.state.length-1) ? this.num = 0 : this.num = (this.num || 0) + 1;
+		return config.state[ind]
+	}
+
+    app.get('/api/getstate', function(req, res) {
+    	res.send(getState());
+    });
+}
+
